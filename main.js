fa4d19081c11c73697b24fe0fff04107210d6f90
timothe_chauvet <timothe_chauvet@ext.carrefour.com>
2026-09-21 21:47:03 +0200

    feat: Meta Ads

--- main.js
+++ main.js
@@ -16,6 +16,38 @@
 
   const BASE_AMAZON_URL = CONFIG.amazonUrl || 'https://www.amazon.com/dp/B0GWC43WN4';
 
+  let metaPixelInitialized = Boolean(window.__MBCT_META_PIXEL_INITIALIZED);
+
+  function enableMetaPixel() {
+    if (metaPixelInitialized || window['fbq-disabled']) return;
+    const pixelId = new URLSearchParams(window.location.search).get('pixel_id') || CONFIG.pixelId;
+    if (!pixelId || pixelId === 'YOUR_PIXEL_ID') return;
+
+    const initialize = () => {
+      if (typeof window.fbq !== 'function') return;
+      window.fbq('init', pixelId);
+      window.fbq('track', 'PageView');
+      metaPixelInitialized = true;
+      window.__MBCT_META_PIXEL_INITIALIZED = true;
+    };
+
+    if (typeof window.fbq === 'function') {
+      initialize();
+      return;
+    }
+
+    window['fbq-disabled'] = false;
+    !function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments); };
+      if (!f._fbq) f._fbq = n;
+      n.push = n; n.loaded = true; n.version = '2.0'; n.queue = [];
+      t = b.createElement(e); t.async = true; t.src = v;
+      s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s);
+    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    initialize();
+  }
+
   let openAIConsent = false;
   let openAIInitialized = false;
 
@@ -901,6 +933,7 @@
     } catch (e) { }
 
     setOpenAIConsent(consent === 'accepted');
+    if (consent === 'accepted') enableMetaPixel();
 
     // If previously declined, disable Meta Pixel immediately
     if (consent === 'declined') {
@@ -928,6 +961,7 @@
           localStorage.setItem(STORAGE_KEY, 'accepted');
         } catch (e) { }
         setOpenAIConsent(true);
+        enableMetaPixel();
         hideBanner();
         trackEvent('CookieConsentAccepted');
       });
